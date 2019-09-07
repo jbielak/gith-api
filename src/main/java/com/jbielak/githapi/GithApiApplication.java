cdd32518c47587f71777124a369b07907ccda551
jbielak <justyna_bielak@outlook.com>
2019-09-07 12:41:04 +0200

    Initial commit

---
+++ src/main/java/com/jbielak/githapi/GithApiApplication.java
@@ -0,0 +1,13 @@
+package com.jbielak.githapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithApiApplication.class, args);
+    }
+
+}
